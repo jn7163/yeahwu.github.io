918aa89efa3ef497de1f5bd65a3e8cf774a52ea0
root <root@Will-PC.localdomain>
2019-09-09 13:46:26 +0800

    Site updated: 2019-09-09 13:46:24

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,1 +1,59 @@
-window.addEventListener("DOMContentLoaded",function(){"use strict";function n(){localStorage.setItem("bookmark"+location.pathname,window.scrollY)}function e(){var e=localStorage.getItem("bookmark"+location.pathname);e=parseInt(e,10),isNaN(e)||""!==location.hash||window.anime({targets:document.documentElement,duration:200,easing:"linear",scrollTop:e})}var o,t;o=CONFIG.bookmark.save,t=document.querySelector(".book-mark-link"),window.addEventListener("scroll",function(){0===window.scrollY?t.classList.add("book-mark-link-fixed"):t.classList.remove("book-mark-link-fixed")}),"auto"===o&&(window.addEventListener("beforeunload",n),window.addEventListener("pjax:send",n)),t.addEventListener("click",function(e){e.preventDefault(),n(),window.anime({targets:t,duration:200,easing:"linear",top:-30,complete:function(){setTimeout(function(){t.style.top=""},400)}})}),e(),window.addEventListener("pjax:success",e)});
+/* global CONFIG */
+
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  var doSaveScroll = () => {
+    localStorage.setItem('bookmark' + location.pathname, window.scrollY);
+  };
+
+  var scrollToMark = () => {
+    var top = localStorage.getItem('bookmark' + location.pathname);
+    top = parseInt(top, 10);
+    // If the page opens with a specific hash, just jump out
+    if (!isNaN(top) && location.hash === '') {
+      // Auto scroll to the position
+      window.anime({
+        targets  : document.documentElement,
+        duration : 200,
+        easing   : 'linear',
+        scrollTop: top
+      });
+    }
+  };
+  // Register everything
+  var init = function(trigger) {
+    // Create a link element
+    var link = document.querySelector('.book-mark-link');
+    // Scroll event
+    window.addEventListener('scroll', () => {
+      window.scrollY === 0 ? link.classList.add('book-mark-link-fixed') : link.classList.remove('book-mark-link-fixed');
+    });
+    // Register beforeunload event when the trigger is auto
+    if (trigger === 'auto') {
+      // Register beforeunload event
+      window.addEventListener('beforeunload', doSaveScroll);
+      window.addEventListener('pjax:send', doSaveScroll);
+    }
+    // Save the position by clicking the icon
+    link.addEventListener('click', event => {
+      event.preventDefault();
+      doSaveScroll();
+      window.anime({
+        targets : link,
+        duration: 200,
+        easing  : 'linear',
+        top     : -30,
+        complete: () => {
+          setTimeout(() => {
+            link.style.top = '';
+          }, 400);
+        }
+      });
+    });
+    scrollToMark();
+    window.addEventListener('pjax:success', scrollToMark);
+  };
+
+  init(CONFIG.bookmark.save);
+});
